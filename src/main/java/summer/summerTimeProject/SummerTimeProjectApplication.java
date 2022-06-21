bed1a59a61aaacefc29c026518c1467af9e57a17
Gahyun Lee <ab3380@naver.com>
2022-06-21 20:17:14 +0900

    first commit

---
+++ src/main/java/summer/summerTimeProject/SummerTimeProjectApplication.java
@@ -0,0 +1,14 @@
+package summer.summerTimeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerTimeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerTimeProjectApplication.class, args);
+		System.out.println("success");
+	}
+
+}
---
+++ src/test/java/summer/summerTimeProject/SummerTimeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package summer.summerTimeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerTimeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
